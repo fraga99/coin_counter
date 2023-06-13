9799b0221fd9b10336340e21d000bfa8b9140411
Diogo Avila Fraga <diogo_avila99@hotmail.com>
2023-06-13 11:12:13 -0300

    arrumar a funçao mostrar

--- contador.c
+++ contador.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
- // Variaveis Globais 
-float total;
-int c05, c10, c25, c50, rl1;
-
-void entrada();
-
-
-main(){   
-    entrada();
+void menu(){
+    int n;
+    printf ("1- Adicionar os valores\n2- Mostrar valores\n0- Sair  ");
+    printf ("\nDigite a opcao desejada: ");
+    scanf ("%d", &n);
+    switch(n){
+        case 0:
+        printf ("Saindo...");
+        break;
+        case 1:
+        entrada();
+        break;
+        case 2:
+        mostrar();
+        break;
+        default:
+        printf ("Valor invalido!\n Por favor, digite o numero da operacao desejada.\n");
+        menu();
+        break;
+    return 0;
     
+    }
 }
 
-void entrada(){
+void mostrar (float total, int c05, int c10, int c25, int c50, int rl1 ){
+    
+    printf ("\n\n########## TOTAL DE CADA MOEDA ##########\n\n");
+    printf("\n05 centavos: R$ %.2f", c05*0.05);
+    printf("\n10 centavos: R$ %.2f", c10*0.1);
+    printf("\n25 centavos: R$ %.2f", c25*0.25);
+    printf("\n50 centavos: R$ %.2f", c50*0.5);
+    printf("\n01 real: R$ %.2f", rl1*1.0);
+    printf ("\n\n########## DINHEIRO TOTAL ##########\n");
+    printf ("\nTotal: R$ %.2f\n", total);
+
+menu();
+}
+
+void entrada(float total, int c05, int c10, int c25, int c50, int rl1){
 printf ("\n\n########## CONTADOR DE MOEDAS ##########\n\n");
 printf ("Digite a quantidade de moedas de 5 centavos: ");
 scanf("%d", &c05);
@@ -26,20 +52,11 @@ scanf("%d", &c50);
 printf ("Digite a quantidade de moedas de 1 real: ");
 scanf("%d", &rl1);
 
-printf ("\n\n########## TOTAL DE CADA MOEDA ##########\n\n");
-printf("\n05 centavos: R$ %.2f", c05*0.05);
-printf("\n10 centavos: R$ %.2f", c10*0.1);
-printf("\n25 centavos: R$ %.2f", c25*0.25);
-printf("\n50 centavos: R$ %.2f", c50*0.5);
-printf("\n01 real: R$ %.2f", rl1*1.0);
-
 total = (c05*0.05 + c10*0.1 + c25*0.25 + c50*0.5 + rl1*1.0);
+menu();
+}
 
+main(){   
+    menu();
 
-printf ("\n\n########## DINHEIRO TOTAL ##########\n");
-printf ("\nTotal: R$ %.2f\n", total);
-
-
-
-return;
 }
